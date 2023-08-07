46f22726250e77365d49d382657a97c31a2559fb
Kevin Sasse <sassek70@gmail.com>
2023-08-07 00:40:41 -0400

    commit tree 6a9ca5a6b06aafe3f6b3bfcda14c08053864a188

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ function upperCaseCustomerName(){
 
 var bestCustomer; 
 
-function setBestCustomer() {
-     bestCustomer = 'not bob';
-}
+// function setBestCustomer() {
+//      bestCustomer = 'not bob';
+// }
 
-function overwriteBestCustomer(){
-    bestCustomer = 'maybe bob'
-}
+// function overwriteBestCustomer(){
+//     bestCustomer = 'maybe bob'
+// }
 
 const leastFavoriteCustomer = "joe"
 
